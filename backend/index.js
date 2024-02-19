30ce87d127f2c4712f0e0d8dda7d575ccab4f2ad
Omkar2310 <mankar.omkar2310@gmail.com>
2024-02-19 14:01:00 +0530

    connected to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Mongo Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
-
-// app.get("/", (req, res) => {
-//   console.log("Home");
-// });
 
 app.listen(3000, () => {
   console.log("====================================");
